d4a69b0cddce91e8c2e158adeed3b3a23dd36389
Stefan Secrieru <secrieru2302@gmail.com>
2025-12-09 14:33:57 +0200

    Fixed vertical overflow

--- src/components/imports/aside/aside.jsx
+++ src/components/imports/aside/aside.jsx
@@ -6,13 +6,13 @@ import {getPatient} from "../../../store/selectors/patientSelectors.js";
 import {Link} from "react-router";
 const Aside = () => {
     const patient = useSelector(getPatient)
-    const patientFamilyName = patient.resource.name[0].family
-    const patientGivenName = patient.resource.name[0].given.join(', ')
+    // const patientFamilyName = patient.resource.name[0].family
+    // const patientGivenName = patient.resource.name[0].given.join(', ')
     return (
         <aside className='aside module'>
             <Filters />
             <div className="aside-double-item">
-                <p className='current-patient'>{patientFamilyName}, {patientGivenName}</p>
+                {/*<p className='current-patient'>{patientFamilyName}, {patientGivenName}</p>*/}
                 <div className="aside-item">
                     <img src={changeSVG} className='svg-icon' alt=""/>
                     <Link to='/patients'>Change Patient</Link>
